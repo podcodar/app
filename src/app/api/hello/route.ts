311a5c4573fd839a240c5d7cd50b42a3d7b9de1a
Vitor Schelb <vitoraiala@gmail.com>
2023-05-03 18:09:52 -0300

    fix: fixed eslint issues

--- src/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
-}
-
-export async function POST(request: Request) {
-  return new Response(request.body);
-}
