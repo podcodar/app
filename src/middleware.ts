a67ccacec977008a3e72b4efe63a36985ea80327
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-11 20:16:54 -0300

    enh: share auth utils
    - share origin key
    - update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import { ORIGIN_URL_KEY } from "@/shared/auth";
 
 export function middleware(request: NextRequest) {
   // add origin path to redirect when not session available
   const headers = new Headers(request.headers);
-  headers.set("x-url", request.nextUrl.pathname);
+  headers.set(ORIGIN_URL_KEY, request.nextUrl.pathname);
 
   return NextResponse.next({
     request: { headers }
--- src/shared/auth.ts
+++ src/shared/auth.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { githubCredentials } from "./settings";
@@ -8,3 +9,12 @@ export const authOptions: NextAuthOptions = {
     signIn: "/login"
   }
 };
+
+export function getOriginPath() {
+  const headerObj = headers();
+  const origin = headerObj.get(ORIGIN_URL_KEY) ?? "";
+
+  return origin;
+}
+
+export const ORIGIN_URL_KEY = "x-url";
