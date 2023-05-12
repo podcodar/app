ca510c6c4b40e32d75b758a33a920d11f4600f30
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-12 17:48:45 -0300

    enh: use sign in form component on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
-import GithubSignInButton from "@/components/GithubSignInButton";
-import SignOutButton from "@/components/SignOutButton";
+import SignInForm from "@/components/SignInForm";
 
 const SignInPage = () => {
   return (
-    <section className='flex flex-col gap-8 items-center justify-center'>
-      <GithubSignInButton />
-      <SignOutButton />
+    <section className='flex flex-col gap-8 items-center justify-center bg-gray-800'>
+      <SignInForm />
     </section>
   );
 };
