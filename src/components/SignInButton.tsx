43cf0ca9f731cb4fc34019349ba48d75acd16e7b
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-16 10:54:42 -0300

    fix: remove unnecessary callbackUrl

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -5,18 +5,15 @@ import { signIn } from "next-auth/react";
 import { ReactNode } from "react";
 
 type PropsType = {
-  children: ReactNode
-  provider: string
-}
+  children: ReactNode;
+  provider: string;
+};
 
-const SignInButton = ({children, provider}: PropsType) => {
-  const searchParams = useSearchParams();
-  const callbackUrl  = searchParams.get("callbackUrl");
-
+const SignInButton = ({ children, provider }: PropsType) => {
   return (
     <button
       className="flex w-full justify-center rounded-md bg-purple-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={() => signIn(provider, { callbackUrl: callbackUrl ?? undefined })}
+      onClick={() => signIn(provider)}
       type="button"
     >
       {children}
