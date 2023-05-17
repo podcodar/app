2fefcebf36ccdb02ea095c2c13cbe9ead64d8d37
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-17 10:11:06 -0300

    enh: remove suffix Type from Types

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,14 +3,14 @@
 import { signIn } from "next-auth/react";
 import { ReactNode } from "react";
 
-type LoginProvidersType = "github"; // | "google"
+type LoginProviders = "github"; // | "google"
 
-type PropsType = {
+type Props = {
   children: ReactNode;
-  provider: LoginProvidersType;
+  provider: LoginProviders;
 };
 
-const SignInButton = ({ children, provider }: PropsType) => {
+const SignInButton = ({ children, provider }: Props) => {
   return (
     <button
       className="flex w-full justify-center rounded-md bg-purple-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
