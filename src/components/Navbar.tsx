4248dc6448b089bb8c47348a47043dda9adccd89
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-17 19:58:15 -0300

    fix: profile options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -89,7 +89,7 @@ export default function Navbar() {
                                 as="button"
                                 className={classes(
                                   active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
+                                  "block px-4 py-2 text-sm text-gray-700 w-full"
                                 )}
                                 onClick={item.onClick}
                               >
