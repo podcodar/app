b9d8693e17b4389a985f9fe3708a504e42446c03
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-18 13:28:18 -0300

    fix: redirect home to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <p className="text-3xl font-bold underline">hello word</p>
-    </main>
-  );
+  return redirect("/login");
 }
