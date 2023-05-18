e5d54ff47d8b8ba1793af3375ab8bf1de5c896e2
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-18 18:08:06 -0300

    fea: setup google credentials

--- src/shared/settings.ts
+++ src/shared/settings.ts
@@ -5,6 +5,18 @@ export const githubCredentials = {
   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
 };
 
+export const googleCredentials = {
+  clientId: process.env.GOOGLE_CLIENT_ID as string,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  authorization: {
+    params: {
+      prompt: "consent",
+      access_type: "offline",
+      response_type: "code",
+    },
+  },
+};
+
 export const ORIGIN_URL_KEY = "x-url";
 export const NO_SESSION_REDIRECT = "/login?callbackUrl=";
 
