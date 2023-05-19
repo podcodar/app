b177d780641970fe159e74dca5353336fdca156b
Vitor Schelb <vitoraiala@gmail.com>
2023-05-19 18:33:07 -0300

    feat: Create form mockup

--- src/app/app/form/page.tsx
+++ src/app/app/form/page.tsx
@@ -1,11 +1,11 @@
+import OnboardingForm from "@/components/OnboardingForm";
+
 export default function Form() {
   return (
     <div className="min-h-full">
       <div className="bg-white shadow">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Formulário
-          </h1>
+          <OnboardingForm />
         </div>
       </div>
     </div>
