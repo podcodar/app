860abc45005c6eff8f407e14312cd40bca1db170
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-05-23 21:05:03 -0300

    fea: custom @apply classes

--- src/components/OnboardingForm.tsx
+++ src/components/OnboardingForm.tsx
@@ -1,24 +1,21 @@
 export default function OnboardingForm() {
   return (
     <div className="absolute h-screen w-screen z-10 top-0 left-0 bg-slate-800 overflow-auto">
-      <form className="bg-[#5d417A] w-full md:w-9/12 mx-auto my-10 p-8 rounded-lg">
+      <form className="bg-purple-dark w-full md:w-9/12 mx-auto my-10 p-8 rounded-lg">
         <div className=" border-gray-900/10">
-          <h2 className="text-base font-semibold leading-7 text-white">
-            Personal Information
+          <h2 className="text-base text-center font-semibold leading-7 text-white">
+            Formulário de Inscrição
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="first-name"
-              >
-                First name
+              <label className="label" htmlFor="nome-social">
+                Nome Social
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="first-name"
                   name="first-name"
                   type="text"
@@ -27,17 +24,27 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-3">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="last-name"
-              >
-                Last name
+              <label className="label" htmlFor="idade">
+                Idade
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  id="last-name"
+                  className="input"
+                  name="last-name"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label className="label" htmlFor="idade">
+                Gênero
+              </label>
+              <div className="mt-2">
+                <input
+                  autoComplete="family-name"
+                  className="input"
                   name="last-name"
                   type="text"
                 />
@@ -45,16 +52,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-4">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="email"
-              >
+              <label className="label" htmlFor="email">
                 Email address
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="email"
                   name="email"
                   type="email"
@@ -63,16 +67,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-3">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="country"
-              >
+              <label className="label" htmlFor="country">
                 Country
               </label>
               <div className="mt-2">
                 <select
                   autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  className="select"
                   id="country"
                   name="country"
                 >
@@ -84,16 +85,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="col-span-full">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="street-address"
-              >
+              <label className="label" htmlFor="street-address">
                 Street address
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="street-address"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="street-address"
                   name="street-address"
                   type="text"
@@ -102,16 +100,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-2 sm:col-start-1">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="city"
-              >
+              <label className="label" htmlFor="city">
                 City
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="address-level2"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="city"
                   name="city"
                   type="text"
@@ -120,16 +115,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-2">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="region"
-              >
+              <label className="label" htmlFor="region">
                 State / Province
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="address-level1"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="region"
                   name="region"
                   type="text"
@@ -138,16 +130,13 @@ export default function OnboardingForm() {
             </div>
 
             <div className="sm:col-span-2">
-              <label
-                className="block text-sm font-medium leading-6 text-white"
-                htmlFor="postal-code"
-              >
+              <label className="label" htmlFor="postal-code">
                 ZIP / Postal code
               </label>
               <div className="mt-2">
                 <input
                   autoComplete="postal-code"
-                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="input"
                   id="postal-code"
                   name="postal-code"
                   type="text"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-dark": "#5d417A",
+      },
+    },
   },
   plugins: [],
 };
