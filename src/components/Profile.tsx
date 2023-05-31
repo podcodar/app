971643d66878595f2918f934be1f82d6dd63b745
nicolasbrandao <nicolas.brandaor@gmail.com>
2023-05-31 17:18:37 -0300

    enh: minor layout changes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -43,12 +43,12 @@ export default function Profile({ session, username }: Props) {
             <UserIcon className="text-white" />
           )}
         </div>
-        <div className="flex flex-col text-white pb-4 lg:pb-0 border-slate-300 w-5/6">
-          <h1 className="text-3xl font-bold tracking-tight">{username}</h1>
-          <p className="text-slate-300">@nicolasbrandao</p>
+        <div className="flex flex-col text-white pb-4 lg:pb-0 border-slate-300">
+          <h1 className="text-3xl font-bold tracking-tight">{name}</h1>
+          <p className="text-slate-300">@{username}</p>
           <p className="mt-4 text-xl">Software Engineer</p>
         </div>
-        <div className="flex flex-col items-start lg:items-center text-white pb-4 lg:pb-0 border-slate-300 w-5/6 min-w-[200px]">
+        <div className="flex flex-col items-start lg:items-center text-white pb-4 lg:pb-0 border-slate-300 min-w-[200px]">
           <ul className="flex flex-col gap-2 list-none my-auto lg:flex-wrap">
             {infoList.map((item) => {
               return (
@@ -60,11 +60,9 @@ export default function Profile({ session, username }: Props) {
             })}
           </ul>
         </div>
-        <div className="flex flex-col justify-start lg:self-start text-white pb-4 lg:pb-0 border-slate-300 w-5/6">
+        <div className="flex flex-col justify-start lg:self-start text-white pb-4 lg:pb-0 border-slate-300">
           <h1 className="text-xl">Conquistas</h1>
           <div className="flex flex-wrap">
-            <StarIcon className="h-12" />
-            <StarIcon className="h-12" />
             <StarIcon className="h-12" />
             <StarIcon className="h-12" />
             <StarIcon className="h-12" />
