7277678b6420ebd45f35706444ffba0738789f7b
nicolasbrandao <nicolas.brandaor@gmail.com>
2023-06-12 09:54:43 -0300

    oth: make schema migrations

---
+++ prisma/migrations/20230607195920_credentials_compliance/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT,
+ALTER COLUMN "name" DROP NOT NULL,
+ALTER COLUMN "username" DROP NOT NULL,
+ALTER COLUMN "roles" SET DEFAULT ARRAY['USER']::"Roles"[];
