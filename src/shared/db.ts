640efe97cce7dea428ab538ab4fce7033f451d98
nicolasbrandao <nicolas.brandaor@gmail.com>
2023-06-19 15:27:39 -0300

    enh: user User instead of AdapterUser

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -1,20 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import { AdapterUser } from "next-auth/adapters";
+import { User } from "next-auth";
 
 export const prisma = new PrismaClient();
 
-export async function createUser(loginUser: AdapterUser) {
+export async function createUser(loginUser: User) {
   const existingUser = await prisma.user.findUnique({
-    where: { email: loginUser.email },
+    where: { email: loginUser.email as string },
   });
 
   if (!existingUser) {
     await prisma.user.create({
       data: {
-        email: loginUser.email,
+        email: loginUser.email as string,
         avatar: loginUser.image as string,
         name: loginUser.name as string,
-        username: loginUser.email,
+        username: loginUser.email as string,
       },
     });
   }
