97fd0120431e4242cf1b44afe299d2fee601002e
nicolasbrandao <nicolas.brandaor@gmail.com>
2023-07-06 10:00:11 -0300

    ref: create stateless context

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -1,84 +1,22 @@
-"use client";
-
-import { ActionType } from "@/shared/types";
 import { User } from "@prisma/client";
-import {
-  Dispatch,
-  ReactNode,
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useReducer,
-} from "react";
+import { ReactNode, createContext, useContext } from "react";
 
-export const userActions = "FETCH_USER";
-
-type State = {
-  user: User;
-};
-
-const initialState: State = {
-  user: {
-    id: 0,
-    avatar: "",
-    email: "",
-    name: "",
-    roles: [],
-    username: "",
-  },
-};
-
-export const reducer = (state: State, action: ActionType<User>) => {
-  switch (action.type) {
-    case userActions:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-export const makeUser = (user: User) => ({
-  type: userActions,
-  payload: user,
-});
-
-export const UserContext = createContext(initialState);
-
-function makeActions(
-  dispatch: Dispatch<ReturnType<typeof makeUser>>,
-  loggedUser: User
-) {
-  return {
-    fetchUser: async () => {
-      dispatch(makeUser(loggedUser));
-    },
-  };
-}
+const UserContext = createContext<User | null>(null);
 
 type Props = {
-  loggedUser: User;
+  user: User;
   children: ReactNode;
 };
 
-export function UserProvider({ loggedUser, children }: Props) {
-  const [user, dispatch] = useReducer(reducer, initialState);
-
-  const actions = useMemo(
-    () => makeActions(dispatch, loggedUser),
-    [dispatch, loggedUser]
-  );
-
-  useEffect(() => {
-    actions.fetchUser();
-  }, [actions]);
-
+export const UserProvider = ({ user, children }: Props) => {
   return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
-}
+};
 
-export function useUser() {
-  return useContext(UserContext);
-}
+export const useUser = (): User => {
+  const user = useContext(UserContext);
+  return user ?? raise("No provider found.");
+};
+
+const raise = (error: string): never => {
+  throw new Error(error);
+};
