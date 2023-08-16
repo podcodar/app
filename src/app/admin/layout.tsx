909d623b1faa197c0e1fbc2f2f328644df88605a
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-16 11:02:29 -0300

    fix: remove unnecessary props

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,26 +1,18 @@
 import { Metadata } from "next";
-import { getServerSession } from "next-auth";
 import { PropsWithChildren } from "react";
 import AuthProvider from "@/contexts/AuthProvider";
-import { authOptions } from "@/shared/auth";
 import Navbar from "@/components/Navbar";
-
-import { fetchUserWithSession } from "@/shared/auth";
 
 export const metadata: Metadata = {
   title: "PodCodar Admin",
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const session = await getServerSession(authOptions);
-
-  const loggedUser = await fetchUserWithSession(session);
-
   return (
     <body>
       <AuthProvider>
         <div className="bg-white shadow">
-          <Navbar loggedUser={loggedUser} />
+          <Navbar />
         </div>
         {children}
       </AuthProvider>
