9b53da357807d662140ec22fd94c3a2e1df18dfd
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-16 11:05:57 -0300

    enh: add UserProvider

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,20 +1,31 @@
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import { PropsWithChildren } from "react";
 import AuthProvider from "@/contexts/AuthProvider";
+import { authOptions } from "@/shared/auth";
 import Navbar from "@/components/Navbar";
+
+import { fetchUserWithSession } from "@/shared/auth";
+import { UserProvider } from "@/contexts/UserProvider";
 
 export const metadata: Metadata = {
   title: "PodCodar Admin",
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
+  const session = await getServerSession(authOptions);
+
+  const loggedUser = await fetchUserWithSession(session);
+
   return (
     <body>
       <AuthProvider>
-        <div className="bg-white shadow">
-          <Navbar />
-        </div>
-        {children}
+        <UserProvider user={loggedUser}>
+          <div className="bg-white shadow">
+            <Navbar />
+          </div>
+          {children}
+        </UserProvider>
       </AuthProvider>
     </body>
   );
