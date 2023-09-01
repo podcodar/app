1a80b63c98de4f052ad34ba2a29348f1249ab424
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-01 12:15:07 -0300

    fea: create checkUserTask DAO method

--- src/shared/tasks.dao.ts
+++ src/shared/tasks.dao.ts
@@ -1,9 +1,36 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "./db";
 
 class TaskDAO {
   async listNoDependenciesTasks() {
     return await prisma.task.findMany({
       where: { dependsOn: { none: {} } },
+    });
+  }
+
+  async fetchTaskById(id: number) {
+    return await prisma.task.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async fetchUserTasksBy(where: Prisma.UserTasksWhereInput) {
+    return await prisma.userTasks.findMany({
+      where,
+      include: {
+        Task: true,
+      },
+    });
+  }
+
+  async checkUserTask(taskId: number, userId: number, completed: boolean) {
+    return prisma.userTasks.update({
+      where: {
+        userId_taskId: { userId, taskId },
+      },
+      data: { completed },
     });
   }
 }
