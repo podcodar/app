7735bd5d9d8df159dc3d4fdbdd8d99c4dff3dfae
Marco Antônio <ma.souza.junior@gmail.com>
2023-10-02 19:13:11 -0300

    fea: unlock dependent tasks

--- src/dao/tasks.dao.ts
+++ src/dao/tasks.dao.ts
@@ -1,11 +1,95 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "./client";
+import { logger } from "@/shared/logger";
 
 class TaskDAO {
   async listNoDependenciesTasks() {
     return await prisma.task.findMany({
       where: { dependsOn: { none: {} } },
     });
+  }
+  async unlockAvailableTasks(taskId: number, userId: number) {
+    const followUpTasks = await prisma.task.findUniqueOrThrow({
+      where: { id: taskId },
+      include: {
+        followUp: {
+          include: {
+            dependentTask: true,
+          },
+        },
+      },
+    });
+
+    logger.log({
+      context: "handleFollowUpTasks",
+      followUpTasks,
+    });
+
+    await Promise.all(
+      followUpTasks.followUp.map(async (followUpTask) => {
+        const { dependentTask } = followUpTask;
+
+        logger.log("Handle follow-up task", {
+          context: "handleFollowUpTasks",
+          followUpTask,
+        });
+
+        const dependencies = await prisma.tasksDependencies.findMany({
+          where: { dependentTask },
+        });
+
+        const validDependencies = dependencies.filter(
+          (t) => t.taskId != taskId
+        );
+
+        logger.log("List all task dependencies", {
+          context: "handleFollowUpTasks",
+          dependencies,
+          validDependencies,
+        });
+
+        const allDependenciesDone = await Promise.all(
+          validDependencies.map(async ({ taskId }) => {
+            const userTask = await prisma.userTasks.findUnique({
+              where: {
+                userId_taskId: { taskId, userId },
+              },
+            });
+
+            return userTask?.completed;
+          })
+        );
+
+        logger.log({
+          context: "handleFollowUpTasks",
+          allDependenciesDone,
+        });
+
+        if (allDependenciesDone.every(Boolean)) {
+          logger.log("Assing a new user task", {
+            context: "handleFollowUpTasks",
+            taskId: dependentTask.id,
+          });
+
+          await prisma.userTasks.upsert({
+            where: {
+              userId_taskId: {
+                userId,
+                taskId: dependentTask.id,
+              },
+            },
+            update: {
+              completed: false,
+            },
+            create: {
+              userId,
+              taskId: dependentTask.id,
+              completed: false,
+            },
+          });
+        }
+      })
+    );
   }
 
   async fetchTaskById(id: number) {
