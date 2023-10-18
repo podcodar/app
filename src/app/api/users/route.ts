ffceba01abc2cf094a55999353636645b9d2c135
Luana Nunes <lununes.s@outlook.com>
2023-10-18 18:42:10 -0300

    feat: create endpoint to get users

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,20 @@
+import { user } from "@/dao/user.dao";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      await user;
+
+      res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
