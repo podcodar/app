773dd48b9fcdfaae7e20624f7439c8e3e06dbf19
Felipe Ogata <82166626+ogatalars@users.noreply.github.com>
2023-11-29 19:38:45 -0300

    change the place on  route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,4 @@
-import prisma from "@/prisma/client";
+import { user } from "@/dao/user.dao";
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
@@ -14,39 +14,9 @@ export default async function handler(
         email: email as string | undefined,
       };
 
-      const users = await prisma.user.findMany({
-        where: {
-          username: filter.username ? { contains: filter.username } : undefined,
-          email: filter.email ? { contains: filter.email } : undefined,
-        },
-        skip: (parseInt(page as string) || 1 - 1) * (parseInt(pageSize as string) || 10),
-        take: parseInt(pageSize as string) || 10,
-      });
-
+      const users = await user.fetchUsers(filter, parseInt(page as string) || 1, parseInt(pageSize as string) || 10);
       res.status(200).json(users);
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else if (req.method === 'POST') {
-    try {
-      const { loginUser } = req.body;
-
-      if (!loginUser) {
-        return res.status(400).json({ error: 'Missing loginUser data' });
-      }
-
-      const createdUser = await prisma.user.create({
-        data: {
-          username: loginUser.email ?? "",
-          email: loginUser.email ?? "",
-          name: loginUser.name ?? "",
-          avatar: loginUser.image ?? undefined,
-        },
-      });
-
-      res.status(201).json(createdUser);
-    } catch (error) {
-      console.error('Error creating user:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   } else {
