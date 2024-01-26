7cd623e4aa0e3c1593458f502ddc6a839ada95ce
Felipe Ogata <82166626+ogatalars@users.noreply.github.com>
2024-01-26 10:38:02 -0300

    fix: route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,29 +1,21 @@
 import { user } from "@/dao/user.dao";
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function GET(req: NextRequest) {
   try {
-    if (req.method === 'GET') {
-      const { username, email, page, pageSize } = req.query;
+    const { username, email, page, pageSize } = req.query;
 
-      const filter = {
-        username: typeof username === 'string' ? username : undefined,
-        email: typeof email === 'string' ? email : undefined,
-      };
+    const filter = {
+      username: typeof username === 'string' ? username : undefined,
+      email: typeof email === 'string' ? email : undefined,
+    };
 
-      const pageNum = parseInt(page as string, 10);
-      const size = parseInt(pageSize as string, 10);
+    const pageNum = parseInt(page as string, 10);
+    const size = parseInt(pageSize as string, 10);
 
-      const users = await user.fetchUsers(filter, isNaN(pageNum) ? 1 : pageNum, isNaN(size) ? 10 : size);
-      res.status(200).json(users);
-    } else {
-      res.setHeader('Allow', ['GET']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+    const users = await user.fetchUsers(filter, isNaN(pageNum) ? 1 : pageNum, isNaN(size) ? 10 : size);
+    return NextResponse.json(users);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
   }
 }
