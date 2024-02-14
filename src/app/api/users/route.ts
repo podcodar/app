f71d396d882b9cc7b46380141bcdaaa86c8985a5
Luana Nunes <lununes.s@outlook.com>
2024-02-14 16:47:56 -0300

    fix: users route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,21 +1,27 @@
 import { user } from "@/dao/user.dao";
-import { NextApiRequest, NextApiResponse, NextResponse } from 'next';
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
 
 export async function GET(req: NextApiRequest) {
   try {
-    const { username, email, page, pageSize } = req.query;
+    const { username, email, page, pageSize } = req.query || {};
 
     const filter = {
-      username: typeof username === 'string' ? username : undefined,
-      email: typeof email === 'string' ? email : undefined,
+      username: typeof username === "string" ? username : undefined,
+      email: typeof email === "string" ? email : undefined,
     };
 
     const pageNum = page ? parseInt(page as string, 10) : 1;
     const size = pageSize ? parseInt(pageSize as string, 10) : 10;
 
     const users = await user.fetchUsers(filter, pageNum, size);
-    return NextResponse.json(users);
+    return new NextResponse(JSON.stringify(users), {
+      status: 201,
+    });
   } catch (error) {
-    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      { status: 500 }
+    );
   }
 }
